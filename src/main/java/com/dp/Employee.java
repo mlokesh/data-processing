e1017cb8812c81468b7221d37beb0b602a2f62b7
Lokesh M <gmlokesh@gmail.com>
2017-05-25 22:01:36 +0530

    test

---
+++ src/main/java/com/dp/Employee.java
@@ -0,0 +1,39 @@
+package com.dp;
+
+public final class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(final int id, final String firstName, final String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/main/java/com/dp/Processor.java
@@ -0,0 +1,96 @@
+package com.dp;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.JdbcRDD;
+import scala.reflect.ClassManifestFactory$;
+import scala.runtime.AbstractFunction0;
+import scala.runtime.AbstractFunction1;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Processor {
+
+    private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String MYSQL_CONNECTION_URL = "jdbc:mysql://localhost:3306/employees";
+    private static final String MYSQL_USERNAME = "admin";
+    private static final String MYSQL_PWD = "admin123";
+
+    public static void main(String[] args) {
+        System.out.println("Invoked Spark");
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("Data Processing");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        DbConnection dbConnection = new DbConnection(MYSQL_DRIVER, MYSQL_CONNECTION_URL, MYSQL_USERNAME, MYSQL_PWD);
+
+        JdbcRDD<Object[]> jdbcRDDOne =
+                new JdbcRDD<>(sc.sc(), dbConnection,
+                        "select * from personsOne where id >= ? and id <= ?",
+                        0,
+                        100000,
+                        1000, new MapResult(), ClassManifestFactory$.MODULE$.fromClass(Object[].class));
+
+        JdbcRDD<Object[]> jdbcRDDTwo =
+                new JdbcRDD<>(sc.sc(), dbConnection,
+                        "select * from personsTwo where id >= ? and id <= ?",
+                        0,
+                        100000,
+                        1000, new MapResult(), ClassManifestFactory$.MODULE$.fromClass(Object[].class));
+
+
+        JavaRDD<Object[]> javaRDDOne = JavaRDD.fromRDD(jdbcRDDOne, ClassManifestFactory$.MODULE$.fromClass(Object[].class));
+        JavaRDD<Object[]> javaRDDTwo = JavaRDD.fromRDD(jdbcRDDOne, ClassManifestFactory$.MODULE$.fromClass(Object[].class));
+
+    }
+
+    static class DbConnection extends AbstractFunction0<Connection> implements Serializable {
+
+        private String driverClassName;
+        private String connectionUrl;
+        private String userName;
+        private String password;
+
+        DbConnection(String driverClassName, String connectionUrl, String userName, String password) {
+            this.driverClassName = driverClassName;
+            this.connectionUrl = connectionUrl;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        @Override
+        public Connection apply() {
+            try {
+                Class.forName(driverClassName);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            Properties properties = new Properties();
+            properties.setProperty("user", userName);
+            properties.setProperty("password", password);
+
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(connectionUrl, properties);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+    }
+
+    static class MapResult extends AbstractFunction1<ResultSet, Object[]> implements Serializable {
+
+        public Object[] apply(ResultSet row) {
+            return JdbcRDD.resultSetToObjectArray(row);
+        }
+    }
+
+}
